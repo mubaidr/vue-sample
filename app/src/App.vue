8e0a3b6f9a8d2573161bd2b0b912d1c27d55ece8
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-12-22 11:10:10 +0500

    Implement json read

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,61 +1,86 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <pre>
+      {{this.content}}
+    </pre>
+    <div class="editable" v-html='content' ref='editable' contenteditable="true" @input="update($event)">{{ content }}</div>
   </div>
 </template>
 
 <script>
+  import _data from './json-data.json'
+
   export default {
     name: 'app',
     data() {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        data: _data,
+        content: 'This Content is <span style="color: red">Editable</span>...'
       }
+    },
+    methods: {
+      update(event) {
+        console.log(event)
+      },
+      convertJsonToHtml(json) {
+        const elem = document.createElement(json.type)
+        const childs = json['inner-elements']
+        // id
+        if (elem.id) {
+          elem.id = json.id
+        }
+        // text
+        if (json.text) {
+          elem.innerText = json.text
+        }
+        // styles
+        if (json.styles) {
+          Object.keys(json.styles).forEach(style => {
+            elem.style[style] = json.styles[style]
+          })
+        }
+        // childs
+        if (childs && childs.length > 0) {
+          childs.forEach(child => {
+            elem.innerHTML += this.convertJsonToHtml(child)
+          })
+        }
+
+        return elem.outerHTML
+      }
+    },
+    mounted() {
+      if (Object.keys(this.data).length === 0) return
+      this.content = this.convertJsonToHtml(this.data)
+    },
+    created() {
+      console.log(this.data)
     }
   }
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  body {
+    background-color: rgba(0, 0, 0, 0.75);
+  }
+  .editable {
+    color: rgb(255, 255, 255);
+    opacity: 1;
+    border-radius: 6px;
+    transform: rotate(0deg);
+    border: 1px solid rgb(70, 144, 247);
+    padding: 10px;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-
-  h1,
-  h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
+    letter-spacing: 0px;
+    font-size: 32px;
+    font-family: Aleo;
+    font-weight: 300;
+    text-decoration: none;
+    box-shadow: rgb(0, 0, 0) 0px 0px 0px 0px;
+    text-transform: initial;
+    font-style: normal;
+    z-index: 2002;
+    text-shadow: rgba(0, 0, 0, 0.2) 2px 2px 0px;
+    line-height: 1.5em;
   }
 </style>
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,5 +1,5 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/main.js',
@@ -12,16 +12,13 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },      {
+        use: ['vue-style-loader', 'css-loader']
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
-          }
+          loaders: {}
           // other vue-loader options go here
         }
       },
@@ -41,7 +38,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      vue$: 'vue/dist/vue.esm.js'
     },
     extensions: ['*', '.js', '.vue', '.json']
   },
